760e79e840325f3d6eaf207602526b8acc592340
Jennifer Ong <jennifer.ex.ong@gmail.com>
2024-08-20 15:08:17 +1200

    Add prettier and husky

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,20 +1,20 @@
-"use client";
+'use client';
 
 /**
  * This configuration is used to for the Sanity Studio that’s mounted on the `/app/admin/[[...tool]]/page.tsx` route
  */
 
-import { visionTool } from "@sanity/vision";
-import { defineConfig } from "sanity";
-import { structureTool } from "sanity/structure";
+import { visionTool } from '@sanity/vision';
+import { defineConfig } from 'sanity';
+import { structureTool } from 'sanity/structure';
 
 // Go to https://www.sanity.io/docs/api-versioning to learn how API versioning works
-import { apiVersion, dataset, projectId } from "./sanity/env";
-import { schema } from "./sanity/schemas";
-import { structure } from "./sanity/structure";
+import { apiVersion, dataset, projectId } from './sanity/env';
+import { schema } from './sanity/schemas';
+import { structure } from './sanity/structure';
 
 export default defineConfig({
-  basePath: "/admin",
+  basePath: '/admin',
   projectId,
   dataset,
   // Add and edit the content schema in the './sanity/schemaTypes' folder
