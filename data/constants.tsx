6d9553c48271bd7b241ff7dca615cc29763accfd
Jennifer Ong <jennifer.ex.ong@gmail.com>
2024-12-13 10:52:19 +1300

    Add card size control to sanity

--- data/constants.tsx
+++ data/constants.tsx
@@ -7,15 +7,14 @@ import Open from '@/assets/icons/open.svg';
 export const DEFAULT_CARD_TEXT_COLOR = '#000000';
 export const DEFAULT_CARD_COLOR = '#ffffff';
 
-export const CARD_RATIO = 1.414;
+export const CARD_RATIO = {
+  square: 1,
+  portrait: 1.414,
+};
+
 export const CARD_TEXT_SIZE_TITLE = 18;
 export const CARD_TEXT_SIZE_HEADING = 16;
 export const CARD_TEXT_SIZE = 12;
-
-export const CARD_WIDTH = 3;
-export const CARD_HEIGHT = CARD_WIDTH * CARD_RATIO;
-export const CARD_WIDTH_PX = 600;
-export const CARD_HEIGHT_PX = CARD_WIDTH_PX * CARD_RATIO;
 
 export const CURSOR_ELEMENT: Record<CardAnimationState, ReactNode> = {
   [CardAnimationState.Closing]: <Label icon={Open} label="Open" />,
