33d12fbd3cba8517f65232c10812229b8d673169
Jenniferexong <jennifer.ex.ong@gmail.com>
2024-12-13 18:36:35 +1300

    Update text color

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-blue">Sentiments</h1>
+      <h1 className="text-white">Sentiments</h1>
     </main>
   );
 }
