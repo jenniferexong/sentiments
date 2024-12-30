73c195f1c0de9c6fbb427aeded71cb785aabb1ff
Jennifer Ong <jennifer.ex.ong@gmail.com>
2024-12-30 19:45:39 +1300

    Add 'Share' button to sanity

--- sanity/components/CardUrlField.tsx
+++ sanity/components/CardUrlField.tsx
@@ -1,16 +1,39 @@
-import { Text } from '@sanity/ui';
+import { ShareIcon } from '@sanity/icons';
+import { Button, Card, Inline, Stack, Text, useToast } from '@sanity/ui';
 import { useFormValue } from 'sanity';
 
 export const CardUrlField: React.FC = () => {
   const id = useFormValue(['_id']);
+  const password = useFormValue(['accessCode']);
 
   const url = `${window.location.origin}/${String(id)}`;
+  const toast = useToast();
 
   return (
-    <Text>
-      <a href={url} target="_blank" rel="noreferrer">
-        {url}
-      </a>
-    </Text>
+    <Card>
+      <Stack space={4}>
+        <Text>
+          <a href={url} target="_blank" rel="noreferrer">
+            {url}
+          </a>
+        </Text>
+        <Inline>
+          <Button
+            onClick={() => {
+              navigator.clipboard.writeText(`${url}\nPassword: ${password}`);
+              toast.push({
+                status: 'warning',
+                title: 'Copied to clipboard',
+              });
+            }}
+            mode="ghost"
+            tone="caution"
+            icon={ShareIcon}
+            padding={[2, 2, 3]}
+            text="Share"
+          />
+        </Inline>
+      </Stack>
+    </Card>
   );
 };
